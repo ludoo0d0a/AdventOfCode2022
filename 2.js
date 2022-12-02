e545e94a1627b709708047c6cb0015fda4b07d61
LudoO <ludovic.valente@gmail.com>
2022-12-02 13:16:35 +0000

    day2 - star2

--- 2.js
+++ 2.js
@@ -4,10 +4,27 @@ import log from 'loglevel';
 //log.setDefaultLevel('debug')
 log.setDefaultLevel('info')
 
+
+// R: A for Rock, B for Paper, and C for Scissors
+// Rock defeats Scissors, Scissors defeats Paper, and Paper defeats Rock.
+// A/X defeats C/Z, C/Z defeats B/Y, and B/Y defeats A/X.
+const wins = {
+    A: 'C',  // A wins over C
+    B: 'A',
+    C: 'B'
+}
+const loses = {
+    A: 'B',  // A lost over B
+    B: 'C',
+    C: 'A'
+}
+
+
 var lines = readFile('2.txt')
 
-// Q: A for Rock, B for Paper, and C for Scissors
-// R: X for Rock, Y for Paper, and Z for Scissors
+// R: A for Rock, B for Paper, and C for Scissors
+// day1 Q: X for Rock, Y for Paper, and Z for Scissors
+// day2 Q: X for lose, Y for draw, and Z for win
 var groups = [];
 var total = 0;
 var group = {weight:0};
@@ -16,8 +33,8 @@ lines.map(line => {
     if (!line) return;
     let values = line.split(' ')
     group = {
-        res: values[0], 
-        req: mapXYZ(values[1])
+        res: values[0], //aka response, the opponent
+        req: values[1]  //aka request, ie me
         //winner: 
         //score: 0
     };
@@ -29,17 +46,42 @@ function mapXYZ(value){
 }
 
 function computeWinner(group, line){
-    const youWin = checkWin(group.req,group.res);
-    const youLose = checkWin(group.res,group.req);
-    //const equalsRound = !youWin && !youLose;
+
+    // day2 Q: X for lose, Y for draw, and Z for win
+    const youWin = (group.req=='Z');
+    const youLose = (group.req=='X');
+    const equalsRound = (group.req=='Y');
+    const shape = getMyShape(group)
+    
+    // const youWin = checkWin(group.req,group.res);
+    // const youLose = checkWin(group.res,group.req);
+    // //const equalsRound = !youWin && !youLose;
+    // const shape = group.req;
 
     // 1 for Rock, 2 for Paper, and 3 for Scissors
-    const extra = (group.req=='A')?1:((group.req=='B')?2:3);
+    const extra = (shape=='A')?1:((shape=='B')?2:3);
     // 0 if you lost, 3 if the round was a draw, and 6 if you won
     const score = youLose?0:(youWin?6:3);
     group.score = extra + score;
     total+=group.score;
     log.debug('%s > score: %d+%d=%d', line, extra, score, group.score)
+}
+
+function getMyShape(group){
+    const youWin = (group.req=='Z');
+    const youLose = (group.req=='X');
+    const equalsRound = (group.req=='Y');
+    const otherShape = group.res;
+    if (equalsRound){
+        return otherShape;
+    }else if (youWin){
+        //return wins[otherShape];
+        return loses[otherShape];
+    }else{
+        // youLose
+        return wins[otherShape];
+        //return loses[otherShape];
+    }
 }
 
 function checkWin(a, b){
