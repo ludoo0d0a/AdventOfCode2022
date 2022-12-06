941f983290a81583031ce65eb92b43c3b80c3a67
LudoO <ludovic.valente@gmail.com>
2022-12-06 21:30:18 +0000

    init cli

--- README.md
+++ README.md
@@ -1,3 +1,25 @@
 # AdventOfCode2022
 
 https://adventofcode.com/2022/
+
+# Get data of the day
+
+```bash
+node init.js init 
+````
+
+or for a specific day
+```bash
+node init.js init -day 6 
+````
+
+# Submit answer
+
+```bash
+node init.js submit -answer 123456
+````
+
+or for a specific day
+```bash
+node init.js submit -day 6 -answer 123456
+````
--- init.js
+++ init.js
@@ -1,61 +1,88 @@
 // source: https://gist.github.com/MathisHammel/0e0e55fcf682086af7d3cd2f66d4a31e
 
+import fs from 'fs';
 import fetch from 'node-fetch';
 import log from 'loglevel';
 import yargs from 'yargs'
 import { hideBin } from 'yargs/helpers'
 
+//
+// Usage:
+// init : 
+//  auto: node init.js init 
+//  day:  node init.js init -day 6 
+//
+// submit : 
+//  auto: node init.js submit 
+//  day:  node init.js submit -day 6 
+//
+
+const AOC_COOKIE='53616c7465645f5f9179cd45826372bee62318a5012ac23473e28f3d0e54abab2b0e274baf19f9c449e407fb34e9dbb463f9ef738685a99bfec5a9c4d33b9fd5';
+const YEAR = '2022' || ((new Date().getFullYear()-2000)) // this year
+// const DAY = argv.day || (new Date().getDate()) //today?
+// const LEVEL = argv.level || 1
 
 const argv = yargs(hideBin(process.argv))
-  .command('init', 'Init day', {
+   .command('init', 'Init day', (yargs) => {
+    return yargs
+  }, (argv) => {
+    if (argv.verbose) console.info(`init on day: ${argv.day}`)
+    getInput(argv.day)
+    getExample(argv.day)
+    console.info(`Day ${argv.day} is prepared !`)
   })
-  .command('submit', 'Submit answer', {
+  .command('submit [answer]', 'Submit answer', (yargs) => {
+    return yargs
+      .positional('answer', {
+        describe: 'Answer to submit'
+      })
+  }, (argv) => {
+    if (argv.verbose) console.info(`Submit answer ${argv.answer} on day: ${argv.day}`)
+    submitAnswer(argv.day, argv.level, argv.answer)
+    console.info(`Answer submitted!`)
   })
   .option('day', {
     alias: 'd',
     description: 'Tell the present day',
-    type: 'number'
+    type: 'number',
+    default: (new Date().getDate()) 
   })
   .option('level', {
     alias: 'l',
     description: 'Tell the level (1 or 2)',
-    type: 'number'
+    type: 'number',
+    default: 1
+  })
+  .option('verbose', {
+    alias: 'v',
+    type: 'boolean',
+    description: 'Run with verbose logging',
+    default: true
   })
   .help()
-  .alias('help', 'h').argv;
-
-const AOC_COOKIE='';
-const YEAR = '2022' || ((new Date().getFullYear()-2000)) // this year
-const DAY = argv.day || (new Date().getDate()) //today?
-const LEVEL = argv.level || 1
-
-if (argv.init){
-    get_input(DAY)
-}else if (argv.submit){
-    //# Your code here
-    ans = 123456
-    submit(DAY, LEVEL, ans)
-}
+  .alias('help', 'h')
+  .parse();
 
 async function getInput(day){
     const req = await fetch(`https://adventofcode.com/${YEAR}/day/${day}/input`, {
             method: 'get',
             headers: {'cookie': `session=${AOC_COOKIE}` }
     });
-    fs.writeFileSync(`data/${DAY}.txt`, req.text)   
+    fs.writeFileSync(`data/${day}.txt`, await req.text())   
 }    
 
-async function getExample(day,offset){
+async function getExample(day, offset){
     if (!offset) offset=0;
     const req = await fetch(`https://adventofcode.com/${YEAR}/day/${day}`, {
             method: 'get',
             headers: {'cookie': `session=${AOC_COOKIE}` }
     });
-    const text = req.text.split('<pre><code>')[offset+1].split('</code></pre>')[0]
-    fs.writeFileSync(`samples/${DAY}.txt`, text)   
+    const text = await req.text();
+    const sample = text.split('<pre><code>')[offset+1].split('</code></pre>')[0]
+    fs.writeFileSync(`samples/${day}.txt`, sample)   
 }
 
-async function submit(day, level, answer){
+async function submitAnswer(day, level, answer){
     log.info('You are about to submit the follwing answer:\n>>>>>>>>>>>>>>>>> {answer}\nPress enter to continue or Ctrl+C to abort.')
     const data = {
       'level': ''+level,
@@ -67,18 +94,19 @@ async function submit(day, level, answer){
             body: data,
             headers: {'cookie': `session=${AOC_COOKIE}` }
     });
+    const text = await response.text();
 
-    if (response.text.contains('You gave an answer too recently')){
+    if (text.contains('You gave an answer too recently')){
         log.error('VERDICT : TOO MANY REQUESTS')
-    }else if (response.text.contains('not the right answer')){
-        if (response.text.contains('too low')){
+    }else if (text.contains('not the right answer')){
+        if (text.contains('too low')){
             log.warn('VERDICT : WRONG (TOO LOW)')
-        } else if (response.text.contains('too high')){
+        } else if (text.contains('too high')){
             log.warn('VERDICT : WRONG (TOO HIGH)')
         } else{
             log.error('VERDICT : WRONG (UNKNOWN)')
         }
-    } else if (response.text.contains('seem to be solving the right level.')) {
+    } else if (text.contains('seem to be solving the right level.')) {
         log.error('VERDICT : INVALID LEVEL')
     }else {
         log.info('VERDICT : OK !')
